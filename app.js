b9453a66c75f23d00322196c0406bd791c4e4da5
Abdulrhman19 <am_alharthi19@hotmail.com>
2021-09-06 21:47:07 +0300

    FIX: Preventing the PacMan from going  through walls

--- app.js
+++ app.js
@@ -53,11 +53,11 @@ document.addEventListener('DOMContentLoaded', () => {
             squares.push(sqaure)
 
             // add layout to the board.
-            if(layout[i] === 0) {
+            if (layout[i] === 0) {
                 squares[i].classList.add('pac-dot')
             } else if (layout[i] === 1) {
                 squares[i].classList.add('wall')
-            } else if(layout[i] === 3) {
+            } else if (layout[i] === 3) {
                 squares[i].classList.add('power-pellet')
             }
         }
@@ -73,18 +73,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const movePacman = (e) => {
         squares[pacmanCurrentIndex].classList.remove('pac-man')
 
-        switch(e.keyCode) {
+        switch (e.keyCode) {
             case 37:
-                if(pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -= 1
+                if (pacmanCurrentIndex % width !== 0 && !squares[pacmanCurrentIndex - 1].classList.contains('wall')) 
+                pacmanCurrentIndex -= 1
                 break
             case 38:
-                if(pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -= width
+                if (pacmanCurrentIndex - width >= 0 && !squares[pacmanCurrentIndex - width].classList.contains('wall')) 
+                pacmanCurrentIndex -= width
                 break
             case 39:
-                if(pacmanCurrentIndex % width < width - 1) pacmanCurrentIndex += 1 
+                if (pacmanCurrentIndex % width < width - 1 && !squares[pacmanCurrentIndex + 1].classList.contains('wall'))
+                pacmanCurrentIndex += 1
                 break
             case 40:
-                if(pacmanCurrentIndex + width < width * width) pacmanCurrentIndex += width
+                if (pacmanCurrentIndex + width < width * width && !squares[pacmanCurrentIndex + width].classList.contains('wall')) 
+                pacmanCurrentIndex += width
                 break
         }
         squares[pacmanCurrentIndex].classList.add('pac-man')
